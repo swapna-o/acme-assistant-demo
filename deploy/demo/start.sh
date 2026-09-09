aa0486d92c3c222b04cc1bc625ffe0c8bac9a1a3
swapna-o <swapnao@gmail.com>
2026-09-08 20:06:18 -0400

    Add files via upload

--- deploy/demo/start.sh
+++ deploy/demo/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
-cd /srv/rag && PORT="${RAG_PORT:-8930}" python webapp.py &
+cd /srv/rag && PORT="${RAG_PORT:-8930}" python3 webapp.py &
 cd /srv/demo && exec node dist/server/server/index.js
